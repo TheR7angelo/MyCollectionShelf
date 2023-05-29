517ba8db426032d2616be59893efa36c41b09c26
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-29 16:54:42 +0200

    update

---
+++ MyCollectionShelf/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenCvSharp;
+
+namespace MyCollectionShelf
+{
+
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var lst = new List<VideoCapture>();
+
+            var i = 0;
+            while (true)
+            {
+                using var capture = new VideoCapture(i);
+                if (!capture.IsOpened()) break;
+                lst.Add(capture);
+                i++;
+                
+            }
+            
+            Console.WriteLine(lst.Count);
+        }
+    }
+}
