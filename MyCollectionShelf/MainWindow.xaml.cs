ecb12d3a5d4fa2868a7df0cd838e7046a8b932d2
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-29 21:16:52 +0200

    add camera control

--- MyCollectionShelf/MainWindow.xaml.cs
+++ MyCollectionShelf/MainWindow.xaml.cs
@@ -1,16 +1,53 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using MyCollectionShelf.Camera.Object.Static_Class;
+using ZXing;
+using ZXing.Common;
 
 namespace MyCollectionShelf
 {
-    public partial class MainWindow
+    public partial class MainWindow : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
+        private DecodingOptions _decodingOption = new();
+
+        public DecodingOptions DecodingOptions
+        {
+            get => _decodingOption;
+            set
+            {
+                _decodingOption = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainWindow()
         {
+            DecodingOptions = new DecodingOptions
+            {
+                TryHarder = true,
+                PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.EAN_13 }
+            };
+
             InitializeComponent();
 
+            VideoPreview.ResultFound += VideoPreviewResultFound;
+            
             var devices = CameraHelper.GetAvailableCameras();
             VideoPreview.StartCamera(devices.First());
+        }
+
+        private void VideoPreviewResultFound(Camera.Ui.Camera sender, Result result)
+        {
+            Console.WriteLine(result.Text);
         }
     }
 }
