7813b4603285d02c711b70056a713f878a835132
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-30 13:26:20 +0200

    clean up

--- MyCollectionShelf.WebApi/Book/OpenLibraryApi.cs
+++ MyCollectionShelf.WebApi/Book/OpenLibraryApi.cs
@@ -1,41 +1,28 @@
 ﻿using MyCollectionShelf.WebApi.Object.Class.Json;
 using MyCollectionShelf.WebApi.Object.Enum;
+using MyCollectionShelf.WebApi.Object.Static_Class;
 using Newtonsoft.Json;
 
 namespace MyCollectionShelf.WebApi.Book;
 
-public class OpenLibraryApi
+public class OpenLibraryApi : IBookApi
 {
     private static Uri BaseInformationIsbnApi => new("https://openlibrary.org/isbn/");
     private static Uri BaseCoverIsbnApi => new("https://covers.openlibrary.org/b/isbn/");
-    private HttpClient Client { get; set; } = new();
     
     private string? UserAgent { get; set; }
     
-    public OpenLibraryApi(string userAgent)
+    public OpenLibraryApi(string? userAgent = null)
     {
         UserAgent = userAgent;
-    }
-    
-    public OpenLibraryApi()
-    {
-        
-    }
-
-    private void SetHttpClient()
-    {
-        Client = new HttpClient();
-        if (UserAgent is null) return;
-        {
-            Client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
-        }
     }
 
     public async Task<OpenLibraryBook?> GetBookInformation(string isbn13)
     {
-        SetHttpClient();
-        Client.BaseAddress = BaseInformationIsbnApi;
-        var message = await Client.GetAsync($"{isbn13}.json");
+        using var client = WebClient.GetWebClient(UserAgent);
+        client.BaseAddress = BaseInformationIsbnApi;
+        
+        var message = await client.GetAsync($"{isbn13}.json");
 
         if (!message.IsSuccessStatusCode) return null;
 
@@ -46,12 +33,12 @@ public class OpenLibraryApi
 
     public async Task GetCovers(string isbn13, string filePath, EOpenLibrarySize openLibrarySize)
     {
-        SetHttpClient();
-        Client.BaseAddress = BaseCoverIsbnApi;
+        using var client = WebClient.GetWebClient(UserAgent);
+        client.BaseAddress = BaseCoverIsbnApi;
 
         var size = openLibrarySize.ToString()[0];
 
-        var message = await Client.GetAsync($"{isbn13}-{size}.jpg");
+        var message = await client.GetAsync($"{isbn13}-{size}.jpg");
         if (!message.IsSuccessStatusCode) return;
 
         var imageBytes = await message.Content.ReadAsByteArrayAsync();
---
+++ MyCollectionShelf.WebApi/Object/Static Class/WebClient.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCollectionShelf.WebApi.Object.Static_Class;
+
+public static class WebClient
+{
+    public static HttpClient GetWebClient(string? userAgent = null)
+    {
+        var client = new HttpClient();
+        
+        if (userAgent is not null) client.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
+
+        return client;
+    }
+}
