a7d7ad66dfbbd9b2d34202f2f5b835fc909f96d8
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-30 21:26:07 +0200

    add DateTimeConverter.cs

---
+++ MyCollectionShelf.WebApi/Object/Class/Json/Converter/DateTimeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Newtonsoft.Json;
+
+namespace MyCollectionShelf.WebApi.Object.Class.Json.Converter;
+
+internal class DateTimeConverter : JsonConverter<DateTime?>
+{
+    private string[] Formats { get; } = { "MMM dd, yyyy", "yyyy" };
+
+    public override void WriteJson(JsonWriter writer, DateTime? value, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override DateTime? ReadJson(JsonReader reader, Type objectType, DateTime? existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        if (reader.TokenType != JsonToken.String) return null;
+        var dateStr = (reader.Value?.ToString() ?? string .Empty).Trim();
+
+        if (string.IsNullOrEmpty(dateStr)) return null;
+
+        if (DateTime.TryParseExact(dateStr, Formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out var dateTimeParse))
+        {
+            return dateTimeParse;
+        }
+        
+        try
+        {
+            dateTimeParse = DateTime.Parse(dateStr);
+            return dateTimeParse;
+        }
+        catch (Exception)
+        {
+            // pass
+        }
+
+        return null;
+    }
+}
