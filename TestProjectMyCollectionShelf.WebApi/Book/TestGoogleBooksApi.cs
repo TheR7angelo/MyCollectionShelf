5be9de389908cb308e7fafc0c7e86843f29edc4c
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-30 13:35:27 +0200

    add test project

---
+++ TestProjectMyCollectionShelf.WebApi/Book/TestGoogleBooksApi.cs
@@ -0,0 +1,13 @@
+﻿namespace TestProjectMyCollectionShelf.WebApi.Book;
+
+public class TestGoogleBooksApi
+{
+    [Fact]
+    private async void GetInformationTest()
+    {
+        const string isbn = "9782072934902";
+
+        var api = new MyCollectionShelf.WebApi.Book.GoogleBooksApi();
+        await api.GetBookInformation(isbn);
+    }
+}
