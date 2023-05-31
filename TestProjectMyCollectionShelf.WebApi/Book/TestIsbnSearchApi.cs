bc424916b0d5f0f001105ee89a330991610c88ce
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-31 13:44:12 +0200

    update

--- TestProjectMyCollectionShelf.WebApi/Book/TestIsbnSearchApi.cs
+++ TestProjectMyCollectionShelf.WebApi/Book/TestIsbnSearchApi.cs
@@ -11,13 +11,13 @@ public class TestIsbnSearchApi
 
         using var client = WebClient.GetWebClient();
         client.BaseAddress = new Uri("https://isbnsearch.org/isbn/");
-        
+
         using var messagePreview = await client.GetAsync(isbn);
 
         if (!messagePreview.IsSuccessStatusCode) return;
 
         var json = await messagePreview.Content.ReadAsStringAsync();
-        
+
         // ESSAI PAS CONCLUANT PAS ASSEZ D'INFORMATION
     }
 }
