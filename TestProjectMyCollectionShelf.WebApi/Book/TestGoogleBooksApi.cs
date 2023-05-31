5dbf8a5349caa564166565a14b3632f6f6c83c93
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-31 09:13:33 +0200

    update test

--- TestProjectMyCollectionShelf.WebApi/Book/TestGoogleBooksApi.cs
+++ TestProjectMyCollectionShelf.WebApi/Book/TestGoogleBooksApi.cs
@@ -1,4 +1,7 @@
-﻿namespace TestProjectMyCollectionShelf.WebApi.Book;
+﻿using MyCollectionShelf.WebApi.Object.Book.Enum;
+using MyCollectionShelf.WebApi.Object.Book.Static_Class;
+
+namespace TestProjectMyCollectionShelf.WebApi.Book;
 
 public class TestGoogleBooksApi
 {
@@ -9,5 +12,9 @@ public class TestGoogleBooksApi
 
         var api = new MyCollectionShelf.WebApi.Book.GoogleBooksApi();
         var book = await api.GetBookInformation(isbn);
+
+        var success = await book?.BookCover.DownloadCover(EBookCoverSize.ExtraLarge, "test.jpg")!;
+        
+        Assert.True(success);
     }
 }
