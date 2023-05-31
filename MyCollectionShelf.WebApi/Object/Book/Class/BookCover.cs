d756729ca3afd727088cc175e7553c9d4da1b2a7
TheR7angelo <raphael.antunes@hotmail.fr>
2023-05-31 09:14:14 +0200

    create book information type

---
+++ MyCollectionShelf.WebApi/Object/Book/Class/BookCover.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyCollectionShelf.WebApi.Object.Book.Class;
+
+public class BookCover : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private Uri? _smallThumbnail;
+
+    public Uri? SmallThumbnail
+    {
+        get => _smallThumbnail;
+        set
+        {
+            _smallThumbnail = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Uri? _thumbnail;
+
+    public Uri? Thumbnail
+    {
+        get => _thumbnail;
+        set
+        {
+            _thumbnail = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Uri? _small;
+
+    public Uri? Small
+    {
+        get => _small;
+        set
+        {
+            _small = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Uri? _medium;
+
+    public Uri? Medium
+    {
+        get => _medium;
+        set
+        {
+            _medium = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Uri? _large;
+
+    public Uri? Large
+    {
+        get => _large;
+        set
+        {
+            _large = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Uri? _extraLarge;
+
+    public Uri? ExtraLarge
+    {
+        get => _extraLarge;
+        set
+        {
+            _extraLarge = value;
+            OnPropertyChanged();
+        }
+    }
+
+}
---
+++ MyCollectionShelf.WebApi/Object/Book/Class/BookInformation.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyCollectionShelf.WebApi.Object.Book.Class;
+
+public class BookInformation : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private string? _isbn13;
+
+    public string? Isbn13
+    {
+        get => _isbn13;
+        set
+        {
+            _isbn13 = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string? _title;
+
+    public string? Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private BookCover? _bookCover;
+
+    public BookCover? BookCover
+    {
+        get => _bookCover;
+        set
+        {
+            _bookCover = value;
+            OnPropertyChanged();
+        }
+    }
+}
