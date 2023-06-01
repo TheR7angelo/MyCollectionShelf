5dfad72a9bd4105e027a5537fa4b0ac7d49ac6ba
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-01 22:02:37 +0200

    add new test

---
+++ TestProjectMyCollectionShelf.Book/Book/TestBookAllApi.cs
@@ -0,0 +1,21 @@
+﻿using MyCollectionShelf.Book;
+using MyCollectionShelf.Book.Object.Static_Class;
+using MyCollectionShelf.WebApi.Object.Book.Enum;
+
+namespace TestProjectMyCollectionShelf.WebApi.Book;
+
+public class TestBookAllApi
+{
+    [Fact]
+    private async void TestGetBookInformation()
+    {
+        const string isbn = "9782820325013";
+
+        var api = new BookAllApi();
+        var book = await api.GetBookInformation(isbn);
+        
+        var success = await book?.BookInformations.BookCover.DownloadCover(EBookCoverSize.ExtraLarge, "test.jpg")!;
+        
+        Assert.True(success);
+    }
+}
