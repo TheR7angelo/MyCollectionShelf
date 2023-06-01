d13c599dddddae82acc42cbe09f3f7d0d0537b34
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-01 22:03:01 +0200

    merge all api result in one object

---
+++ MyCollectionShelf.Book/BookAllApi.cs
@@ -0,0 +1,97 @@
+﻿using MyCollectionShelf.Book.Object.Class;
+
+namespace MyCollectionShelf.Book;
+
+public class BookAllApi
+{
+    private string? UserAgent { get; }
+    
+    public BookAllApi(string? userAgent = null)
+    {
+        UserAgent = userAgent;
+    }
+    
+    public async Task<Object.Class.Book> GetBookInformation(string isbn13)
+    {
+        var book = new Object.Class.Book
+        {
+            BookInformations = new BookInformations
+            {
+                Isbn = isbn13,
+                BookCover = new BookCover()
+            },
+            BookNote = new BookNote()
+        };
+        
+        var apis = new List<IBookApi>
+        {
+            new GoogleBooksApi(UserAgent),
+            new OpenLibraryApi(UserAgent)
+        };
+
+        foreach (var api in apis)
+        {
+            var bookTemp = await api.GetBookInformation(isbn13);
+            if (bookTemp is null) continue;
+
+            Copy(bookTemp.BookInformations, book.BookInformations);
+            Copy(bookTemp.BookNote, book.BookNote);
+        }
+
+        return book;
+    }
+
+    private static void Copy(BookNote tempBookNote, BookNote bookNote)
+    {
+        bookNote.Price ??= tempBookNote.Price;
+    }
+
+    private static void Copy(BookInformations tempInformations, BookInformations bookInformations)
+    {
+        bookInformations.Title ??= tempInformations.Title;
+        bookInformations.Series ??= tempInformations.Series;
+        bookInformations.TomeNumber ??= tempInformations.TomeNumber;
+
+        bookInformations.Summarize ??= tempInformations.Summarize;
+        bookInformations.PublishDate ??= tempInformations.PublishDate;
+        bookInformations.Editor ??= tempInformations.Editor;
+        bookInformations.PageNumber ??= tempInformations.PageNumber;
+        bookInformations.Isbn ??= tempInformations.Isbn;
+        
+        Copy(tempInformations.Authors, bookInformations.Authors);
+        Copy(tempInformations.BookCover, bookInformations.BookCover!);
+        Copy(tempInformations.Genre, bookInformations.Genre);
+    }
+
+    private static void Copy(IReadOnlyCollection<string> tempInformationsGenre, ICollection<string> bookInformationsGenre)
+    {
+        if (tempInformationsGenre.Count.Equals(0)) return;
+
+        foreach (var genre in tempInformationsGenre.Where(genre => !bookInformationsGenre.Contains(genre)))
+        {
+            bookInformationsGenre.Add(genre);
+        }
+    }
+
+    private static void Copy(BookCover? tempBookCover, BookCover bookCover)
+    {
+        if (tempBookCover is null) return;
+
+        bookCover.SmallThumbnail ??= tempBookCover.SmallThumbnail;
+        bookCover.Thumbnail ??= tempBookCover.Thumbnail;
+        bookCover.Small ??= tempBookCover.Small;
+        bookCover.Medium ??= tempBookCover.Medium;
+        bookCover.Large ??= tempBookCover.Large;
+        bookCover.ExtraLarge ??= tempBookCover.ExtraLarge;
+    }
+
+    private static void Copy(IReadOnlyCollection<BookAuthors> tempAuthors, ICollection<BookAuthors> bookAuthors)
+    {
+        if (tempAuthors.Count.Equals(0)) return;
+
+        foreach (var author in tempAuthors.Where(author => !bookAuthors.Contains(author)))
+        {
+            bookAuthors.Add(author);
+        }
+    }
+}
