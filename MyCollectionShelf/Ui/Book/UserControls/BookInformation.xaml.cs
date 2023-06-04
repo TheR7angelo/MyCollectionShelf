67808aedfbbe0c23752075503ed96addbd79a6da
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-04 18:25:50 +0200

    add new button to add or remove author

--- MyCollectionShelf/Ui/Book/UserControls/BookInformation.xaml.cs
+++ MyCollectionShelf/Ui/Book/UserControls/BookInformation.xaml.cs
@@ -1,26 +1,63 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using MyCollectionShelf.Book.Object.Class;
 
 namespace MyCollectionShelf.Ui.Book.UserControls;
 
 public partial class BookInformation
 {
-    public static readonly DependencyProperty BookInformationDataProperty = DependencyProperty.Register(nameof(BookInformationData), typeof(MyCollectionShelf.Book.Object.Class.BookInformations), typeof(BookInformation), new PropertyMetadata(default(MyCollectionShelf.Book.Object.Class.BookInformations)));
+    public static readonly DependencyProperty BookInformationDataProperty =
+        DependencyProperty.Register(nameof(BookInformationData), typeof(BookInformations), typeof(BookInformation),
+            new PropertyMetadata(default(BookInformations)));
 
     public BookInformation()
     {
         InitializeComponent();
     }
 
-    public MyCollectionShelf.Book.Object.Class.BookInformations BookInformationData
+    public BookInformations BookInformationData
     {
-        get => (MyCollectionShelf.Book.Object.Class.BookInformations)GetValue(BookInformationDataProperty);
+        get => (BookInformations)GetValue(BookInformationDataProperty);
         set => SetValue(BookInformationDataProperty, value);
     }
 
     private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
     {
-        BookInformationData.Authors.Add(new MyCollectionShelf.Book.Object.Class.BookAuthors());
-        Console.WriteLine(BookInformationData.Authors.Count);
+        var button = (Button)sender;
+        var content = (char)button.Content;
+        var item = (BookAuthors)button.DataContext;
+
+        if (content == '+')
+        {
+            BookInformationData.Authors.Add(new BookAuthors());
+        }
+        else
+        {
+            BookInformationData.Authors.Remove(item);
+        }
+    }
+}
+
+internal class ItemToImageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var listBoxItem = (ListBoxItem)value;
+
+        var sourceItems = (ObservableCollection<BookAuthors>)listBoxItem.Tag;
+        var item = (BookAuthors)listBoxItem.DataContext;
+
+        var index = sourceItems.IndexOf(item);
+
+        return index == 0 ? '+' : '-';
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
     }
 }
