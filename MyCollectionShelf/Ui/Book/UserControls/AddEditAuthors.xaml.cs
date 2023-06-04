319c616006e7b255963a3c6d100170e8bbdae089
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-04 14:37:14 +0200

    update

---
+++ MyCollectionShelf/Ui/Book/UserControls/AddEditAuthors.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace MyCollectionShelf.Ui.Book.UserControls;
+
+public partial class AddEditAuthors
+{
+    public static readonly DependencyProperty BoxTitleProperty = DependencyProperty.Register(nameof(BoxTitle), typeof(string), typeof(AddEditAuthors), new PropertyMetadata(default(string)));
+    public static readonly DependencyProperty BoxTextProperty = DependencyProperty.Register(nameof(BoxText), typeof(string), typeof(AddEditAuthors), new PropertyMetadata(default(string)));
+
+    public AddEditAuthors()
+    {
+        InitializeComponent();
+    }
+
+    public string BoxTitle
+    {
+        get => (string)GetValue(BoxTitleProperty);
+        set => SetValue(BoxTitleProperty, value);
+    }
+
+    public string BoxText
+    {
+        get => (string)GetValue(BoxTextProperty);
+        set => SetValue(BoxTextProperty, value);
+    }
+}
