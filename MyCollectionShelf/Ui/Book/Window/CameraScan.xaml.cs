215a805b5db52843720247602622d5b6268d908d
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-07 10:43:19 +0200

    add camera scanner

---
+++ MyCollectionShelf/Ui/Book/Window/CameraScan.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MyCollectionShelf.Book;
+using MyCollectionShelf.Camera.Object.Static_Class;
+using ZXing;
+using ZXing.Common;
+
+namespace MyCollectionShelf.Ui.Book.Window;
+
+public partial class CameraScan : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private DecodingOptions _decodingOption = new();
+
+    public DecodingOptions DecodingOptions
+    {
+        get => _decodingOption;
+        set
+        {
+            _decodingOption = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private MyCollectionShelf.Book.Object.Class.Book _book = new();
+
+    public MyCollectionShelf.Book.Object.Class.Book Book
+    {
+        get => _book;
+        set
+        {
+            _book = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ObservableCollection<string> IsbnFounds { get; } = new();
+
+    public CameraScan()
+    {
+        IsbnFounds.Add("9782072934902");
+        InitializeComponent();
+
+        DecodingOptions = new DecodingOptions
+        {
+            TryHarder = true,
+            PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.EAN_13 }
+        };
+
+        VideoPreview.ResultFound += VideoPreviewResultFound;
+
+        var devices = CameraHelper.GetAvailableCameras();
+        VideoPreview.StartCamera(devices.First());
+    }
+
+    private void VideoPreviewResultFound(Camera.Ui.Camera sender, Result result)
+    {
+        if (result.BarcodeFormat != BarcodeFormat.EAN_13) return;
+
+        IsbnFounds.Add(result.Text);
+    }
+
+    private async void UIElement_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        var isbn = ((Label)sender).Content as string;
+        Console.WriteLine(isbn);
+        
+        // todo à finir
+        
+        var api = new BookAllApi();
+        var book = await api.GetBookInformation(isbn!);
+        
+        Console.WriteLine(book.BookInformations.Title);
+
+        // var api = new OpenLibraryApi();
+        //
+        // var book = await api.GetBookInformation(result.Text);
+        //
+        // var success = await book?.BookInformations.BookCover.DownloadCover(EBookCoverSize.ExtraLarge,
+        //     $"{book.BookInformations.Title}.jpg")!;
+        //
+        // Console.WriteLine($"cover download {success}");
+    }
+}
