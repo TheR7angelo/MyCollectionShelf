e1de429f39ed501de1cc0e5f04b2f9aa9344842e
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-08 21:11:04 +0200

    add new converter

---
+++ MyCollectionShelf/Object/Converter/UriToImageSourceConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MyCollectionShelf.Object.Converter;
+
+public class UriToImageSourceConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not Uri uri) return DependencyProperty.UnsetValue;
+        try
+        {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = uri;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+        catch (Exception ex)
+        {
+            // Gérer l'exception (par exemple, journaliser l'erreur)
+            Console.WriteLine($"Erreur lors de la conversion de l'URI en ImageSource : {ex.Message}");
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
