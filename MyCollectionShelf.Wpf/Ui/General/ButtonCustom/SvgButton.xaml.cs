33dacf719be656c142334c9c8cc923ba007bb350
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-18 16:49:37 +0200

    create new svgbutton

---
+++ MyCollectionShelf.Wpf/Ui/General/ButtonCustom/SvgButton.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace MyCollectionShelf.Wpf.Ui.General.ButtonCustom;
+
+public partial class SvgButton
+{
+    public static readonly DependencyProperty ImageUriProperty = DependencyProperty.Register(nameof(ImageUri),
+        typeof(Uri), typeof(SvgButton), new PropertyMetadata(default(Uri)));
+
+    public SvgButton()
+    {
+        InitializeComponent();
+    }
+
+    public Uri ImageUri
+    {
+        get => (Uri)GetValue(ImageUriProperty);
+        set => SetValue(ImageUriProperty, value);
+    }
+}
