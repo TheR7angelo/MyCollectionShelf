ef9ce51bfaa38d8eab6fa44d8a7fca4848afea9a
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-18 20:02:21 +0200

    create new test

---
+++ TestProjectMyCollectionShelf.Sql/UnitTest1.cs
@@ -0,0 +1,16 @@
+using MyCollectionShelf.Sql;
+using MyCollectionShelf.Sql.Object.Book.Class;
+
+namespace TestProjectMyCollectionShelf.Sql;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        using var handler = new SqlMainHandler();
+        var db = handler.GetSqlConnection();
+        
+        db.Execute(new BookAuthors().Definition);
+    }
+}
