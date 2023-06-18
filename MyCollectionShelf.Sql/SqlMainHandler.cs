065c5d9f3c67db5b36b11df82e90bebb7b84de2f
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-18 20:02:29 +0200

    add SqlMainHandler.cs

---
+++ MyCollectionShelf.Sql/SqlMainHandler.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace MyCollectionShelf.Sql;
+
+public class SqlMainHandler : IDisposable
+{
+    private readonly SQLiteConnection _sqLiteConnection = new("MyCollectionShelf.sqlite",
+        SQLiteOpenFlags.Create | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.ReadWrite);
+
+    public void Dispose()
+    {
+        _sqLiteConnection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public SQLiteConnection GetSqlConnection() => _sqLiteConnection;
+}
