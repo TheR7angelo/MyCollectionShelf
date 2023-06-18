de122653e470f21c9f8b7fa2bcd6a1c0385c9280
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-18 12:20:41 +0200

    move file

--- MyCollectionShelf.Wpf/Ui/General/UserControls/TextBoxAnimated.xaml.cs
+++ MyCollectionShelf.Wpf/Ui/General/UserControls/TextBoxAnimated.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 
-namespace MyCollectionShelf.Wpf.Ui.General;
+namespace MyCollectionShelf.Wpf.Ui.General.UserControls;
 
 public partial class TextBoxAnimated
 {
