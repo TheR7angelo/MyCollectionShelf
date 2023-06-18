b2488c014a663fb7b8552fc5e72fc52872183336
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-18 22:47:09 +0200

    add BookSeries

---
+++ MyCollectionShelf.Sql/Object/Book/Class/BookSeries.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SQLite;
+
+namespace MyCollectionShelf.Sql.Object.Book.Class;
+
+public class BookSeries : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private long _id;
+
+    [PrimaryKey, AutoIncrement, Column("id")]
+    public long Id
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private string? _title;
+
+    [Column("title")]
+    public string? Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private Uri? _seriesCover;
+
+    [Column("series_cover")]
+    public Uri? SeriesCover
+    {
+        get => _seriesCover;
+        set
+        {
+            _seriesCover = value;
+            OnPropertyChanged();
+        }
+    }
+
+}
