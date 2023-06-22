80ebb29fd1ce741e347b34647befc8db2823eed9
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-22 10:13:06 +0200

    add class RegexFunction.cs

---
+++ MyCollectionShelf.Wpf/Shelf/Book/Object/Class/Static/RegexFunction.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyCollectionShelf.Wpf.Shelf.Book.Object.Class.Static;
+
+public static partial class RegexFunction
+{
+    [GeneratedRegex("[^0-9]+")]
+    private static partial Regex IsNumberRegex();
+
+    public static bool IsNumber(this string str) => IsNumberRegex().IsMatch(str);
+}
