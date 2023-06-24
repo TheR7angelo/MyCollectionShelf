e58d06e32c16de11c2b47250d82d30a2c3100f3c
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-24 15:00:48 +0200

    update ButtonShelfBook.xaml

---
+++ MyCollectionShelf.Wpf/Shelf/Book/Ui/UserControls/CustomButton/ButtonShelfBook.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using MyCollectionShelf.Sql.Object.Book.Class.View;
+
+namespace MyCollectionShelf.Wpf.Shelf.Book.Ui.UserControls.CustomButton;
+
+public partial class ButtonShelfBook
+{
+    public static readonly DependencyProperty VBookShelfProperty = DependencyProperty.Register(nameof(VBookShelf),
+        typeof(VBookShelf), typeof(ButtonShelfBook), new PropertyMetadata(default(VBookShelf)));
+
+    public ButtonShelfBook()
+    {
+        InitializeComponent();
+    }
+
+    public VBookShelf VBookShelf
+    {
+        get => (VBookShelf)GetValue(VBookShelfProperty);
+        set => SetValue(VBookShelfProperty, value);
+    }
+}
