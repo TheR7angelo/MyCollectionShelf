f76a836563caf1ee777e5534b58e09f63f6db977
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-24 15:23:25 +0200

    add some test

--- MyCollectionShelf.Wpf/Shelf/Book/Ui/Pages/BookShelf.xaml.cs
+++ MyCollectionShelf.Wpf/Shelf/Book/Ui/Pages/BookShelf.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using MyCollectionShelf.Sql.Object.Book.Class.View;
 using MyCollectionShelf.Wpf.Shelf.Book.Object.Class.Static;
 
@@ -7,11 +9,20 @@ namespace MyCollectionShelf.Wpf.Shelf.Book.Ui.Pages;
 public partial class BookShelf
 {
     public ObservableCollection<VBookShelf> BookShelves;
+    
     public BookShelf()
     {
-        var lst = CommonUi.SetCollection<VBookShelf>();
-        BookShelves = new ObservableCollection<VBookShelf>(lst);
+        var vBookShelves = CommonUi.SetCollection<VBookShelf>().ToList();
+        
+        foreach (var vBookShelf in vBookShelves)
+        {
+            vBookShelf.BookSeriesCover = Path.GetFullPath(vBookShelf.BookSeriesCover);
+        }
+
+        BookShelves = new ObservableCollection<VBookShelf>(vBookShelves);
         
         InitializeComponent();
+
+        ShelfBook.VBookShelf = BookShelves.First();
     }
 }
