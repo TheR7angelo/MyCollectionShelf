c2e7f16559b269fd9aac33f4d792dc9b44956fe1
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-25 13:30:39 +0200

    add navigator handler

--- MyCollectionShelf.Wpf/Shelf/HomePage.xaml.cs
+++ MyCollectionShelf.Wpf/Shelf/HomePage.xaml.cs
@@ -1,11 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace MyCollectionShelf.Wpf.Shelf;
 
-public partial class HomePage : Page
+public partial class HomePage
 {
     public HomePage()
     {
         InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        var bookPage = new Book.Ui.Pages.BookShelf();
+        bookPage.Navigate();
     }
 }
