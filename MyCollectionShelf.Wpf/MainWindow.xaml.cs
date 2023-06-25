bf4ce29fb766fa850d6e0f370f9d991b469761e8
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-25 12:41:00 +0200

    add home page

--- MyCollectionShelf.Wpf/MainWindow.xaml.cs
+++ MyCollectionShelf.Wpf/MainWindow.xaml.cs
@@ -1,15 +1,10 @@
-﻿using MyCollectionShelf.Wpf.Shelf.Book.Ui.Pages;
-
-namespace MyCollectionShelf.Wpf
+﻿namespace MyCollectionShelf.Wpf
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            // Frame.Content = new AddEditBook();
-            Frame.Content = new BookShelf();
         }
     }
 }
---
+++ MyCollectionShelf.Wpf/Shelf/HomePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MyCollectionShelf.Wpf.Shelf;
+
+public partial class HomePage : Page
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
