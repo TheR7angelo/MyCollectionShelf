25ab1d70b5f94a242083abd17ff3cbe62e207b09
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-25 17:16:46 +0200

    add new converter

---
+++ MyCollectionShelf.Wpf/Shelf/Book/Converter/JoinBookAuthors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using MyCollectionShelf.Sql.Object.Book.Class.Table;
+
+namespace MyCollectionShelf.Wpf.Shelf.Book.Converter;
+
+public class JoinBookAuthors : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null) return string.Empty;
+
+        var values = (IEnumerable<BookAuthor>)value;
+        var results = string.Join("; ", values.Select(s => s.NameConcat));
+
+        return results;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
