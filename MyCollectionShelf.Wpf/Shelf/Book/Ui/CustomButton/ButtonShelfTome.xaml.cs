ec2d71996091aadc600c53f842bf7075c4c723b2
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-25 16:59:44 +0200

    start binding

--- MyCollectionShelf.Wpf/Shelf/Book/Ui/CustomButton/ButtonShelfTome.xaml.cs
+++ MyCollectionShelf.Wpf/Shelf/Book/Ui/CustomButton/ButtonShelfTome.xaml.cs
@@ -1,9 +1,22 @@
-﻿namespace MyCollectionShelf.Wpf.Shelf.Book.Ui.CustomButton;
+﻿using System.Windows;
+using MyCollectionShelf.Sql.Object.Book.Class.Table;
+
+namespace MyCollectionShelf.Wpf.Shelf.Book.Ui.CustomButton;
 
 public partial class ButtonShelfTome
 {
+    public static readonly DependencyProperty BookInformationsProperty =
+        DependencyProperty.Register(nameof(BookInformations), typeof(BookInformations), typeof(ButtonShelfTome),
+            new PropertyMetadata(default(BookInformations)));
+
     public ButtonShelfTome()
     {
         InitializeComponent();
+    }
+
+    public BookInformations BookInformations
+    {
+        get => (BookInformations)GetValue(BookInformationsProperty);
+        set => SetValue(BookInformationsProperty, value);
     }
 }
