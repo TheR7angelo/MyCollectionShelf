4b5bc8e6f6fa22b08f9f7301ea5d82c2ec4c892f
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-27 09:15:27 +0200

    add bug

--- MyCollectionShelf.Wpf/Shelf/Book/Ui/CustomButton/ButtonShelfBook.xaml.cs
+++ MyCollectionShelf.Wpf/Shelf/Book/Ui/CustomButton/ButtonShelfBook.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using MyCollectionShelf.Sql.Object.Book.Class.View;
 
 namespace MyCollectionShelf.Wpf.Shelf.Book.Ui.CustomButton;
@@ -17,5 +18,10 @@ public partial class ButtonShelfBook
     {
         get => (VBookShelf)GetValue(VBookShelfProperty);
         set => SetValue(VBookShelfProperty, value);
+    }
+
+    private void ToolTip_OnOpened(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine(VBookShelf.SeriesSummarize);
     }
 }
