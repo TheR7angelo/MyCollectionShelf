c35aac4569d17bc56f464e7b4aabb5a12146ccc1
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-28 09:11:58 +0200

    add switch theme for test

--- MyCollectionShelf.Wpf/MainWindow.xaml.cs
+++ MyCollectionShelf.Wpf/MainWindow.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace MyCollectionShelf.Wpf
+﻿using System.Windows;
+using MaterialDesignThemes.Wpf;
+
+namespace MyCollectionShelf.Wpf
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            
+            var palette = new PaletteHelper();
+            var theme = palette.GetTheme();
+            var baseTheme = theme.GetBaseTheme();
+
+            if (baseTheme == BaseTheme.Dark)
+            {
+                ToggleButtonTheme.IsChecked = true;
+            }
+
+        }
+
+        private void ToggleButtonTheme_OnChecked(object sender, RoutedEventArgs e)
+        {
+            var palette = new PaletteHelper();
+            var theme = palette.GetTheme();
+
+            theme.SetBaseTheme(ToggleButtonTheme.IsChecked!.Value ? Theme.Dark : Theme.Light);
+            palette.SetTheme(theme);
         }
     }
 }
