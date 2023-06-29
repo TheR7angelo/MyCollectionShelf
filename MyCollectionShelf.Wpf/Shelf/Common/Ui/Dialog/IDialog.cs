da6a56ec754f58e2c88efeee5998d13e8e64bd61
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-29 13:59:06 +0200

    add some dialog

---
+++ MyCollectionShelf.Wpf/Shelf/Common/Ui/Dialog/IDialog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MyCollectionShelf.Wpf.Shelf.Common.Ui.Dialog;
+
+public interface IDialog
+{
+    public IEnumerable<string>? Extension { get; }
+    
+    public string Filter { get; }
+    
+    public string? GetFile();
+
+    public string? SaveFile();
+}
---
+++ MyCollectionShelf.Wpf/Shelf/Common/Ui/Dialog/ImageDialog.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MyCollectionShelf.Wpf.Shelf.Common.Ui.Dialog;
+
+public class ImageDialog : IDialog
+{
+    public IEnumerable<string> Extension => new[] { ".bmp", ".ico", ".jpg", ".jpeg", ".png" };
+    
+    public string Filter { get; }
+
+    private readonly FileDialog _fileDialog;
+
+    public ImageDialog()
+    {
+        var lst = string.Join(';', Extension.Select(ext => $"*{ext}"));
+        Filter = $"Fichier image ({lst})|{lst}";
+
+        _fileDialog = new FileDialog(Filter, false, "Merci de choisir une image");
+    }
+
+    public string? GetFile() => _fileDialog.GetFile();
+
+    public string? SaveFile() => _fileDialog.SaveFile();
+}
