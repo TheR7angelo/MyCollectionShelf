77de13767eab1b42fa0440101e179cc73776407e
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-29 09:55:50 +0200

    start edit mode

--- MyCollectionShelf.Wpf/Shelf/Book/Ui/Pages/BookShelfSeries.xaml.cs
+++ MyCollectionShelf.Wpf/Shelf/Book/Ui/Pages/BookShelfSeries.xaml.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Input;
 using MyCollectionShelf.Sql.Object.Book.Class.Table;
 using MyCollectionShelf.Sql.Object.Book.Class.View;
 using MyCollectionShelf.Wpf.Shelf.Book.Ui.CustomButton;
+using MyCollectionShelf.Wpf.Shelf.Common.Comparator;
 
 namespace MyCollectionShelf.Wpf.Shelf.Book.Ui.Pages;
 
 public partial class BookShelfSeries
 {
     public static readonly DependencyProperty VBookShelfProperty = DependencyProperty.Register(nameof(VBookShelf),
-        typeof(VBookShelf), typeof(BookShelfSeries), new PropertyMetadata(default(VBookShelf)));
+        typeof(VBookShelf), typeof(BookShelfSeries), new PropertyMetadata(default(VBookShelf), VBookShelfPropertyChanged_Callback));
+
+    private static void VBookShelfPropertyChanged_Callback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var sender = (BookShelfSeries)d;
+        sender.VBookShelfOriginal = sender.VBookShelf.DeepCopy();
+    }
 
     public static readonly DependencyProperty BookInformationsProperty =
         DependencyProperty.Register(nameof(BookInformations), typeof(ObservableCollection<BookInformations>),
             typeof(BookShelfSeries), new PropertyMetadata(default(ObservableCollection<BookInformations>)));
 
+    private VBookShelf? VBookShelfOriginal { get; set; }
+    
     public BookShelfSeries()
     {
         InitializeComponent();
@@ -37,5 +47,26 @@ public partial class BookShelfSeries
     {
         var button = (ButtonShelfTome)sender;
         Console.WriteLine(button.BookInformations.TomeNumber);
+    }
+
+    private void ImageCover_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if ((bool)ToggleEdit.IsChecked!)
+        {
+            Console.WriteLine("hey");
+        }
+        else
+        {
+            Console.WriteLine("nop");
+        }
+    }
+
+    private void ToggleEdit_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        var differences = VBookShelfOriginal.Compare(VBookShelf);
+
+        if (differences.Count.Equals(0)) return;
+        
+        Console.WriteLine("dif");
     }
 }
