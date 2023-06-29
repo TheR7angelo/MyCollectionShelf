87f0f15c0c3f6ab148e8d6bb0f8573c9e930479c
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-29 09:25:24 +0200

    move file

--- MyCollectionShelf.Wpf/Shelf/Book/Converters/JoinBookAuthors.cs
+++ MyCollectionShelf.Wpf/Shelf/Book/Converters/JoinBookAuthors.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Windows.Data;
 using MyCollectionShelf.Sql.Object.Book.Class.Table;
 
-namespace MyCollectionShelf.Wpf.Shelf.Book.Converter;
+namespace MyCollectionShelf.Wpf.Shelf.Book.Converters;
 
 public class JoinBookAuthors : IValueConverter
 {
--- MyCollectionShelf.Wpf/Shelf/Common/Converters/StringToImageSourceConverter.cs
+++ MyCollectionShelf.Wpf/Shelf/Common/Converters/StringToImageSourceConverter.cs
@@ -5,7 +5,7 @@ using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
-namespace MyCollectionShelf.Wpf.Shelf.Common.Ui.Converters;
+namespace MyCollectionShelf.Wpf.Shelf.Common.Converters;
 
 public class StringToImageSourceConverter : IValueConverter
 {
