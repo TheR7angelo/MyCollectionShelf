d18dfcd583be9ca57cfbe1c61ac453ba8498d764
TheR7angelo <raphael.antunes@hotmail.fr>
2023-06-30 15:33:32 +0200

    add sql parser

---
+++ MyCollectionShelf.Sql/Common.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace MyCollectionShelf.Sql;
+
+public static class Common
+{
+    private const string Null = "NULL";
+
+    public static string SqlNull(this string? value) =>
+        string.IsNullOrEmpty(value) ? Null : $"'{value.Replace("'", "''")}'";
+
+    public static string SqlNull(this float? value) =>
+        value is null ? Null : value.Value.ToString(CultureInfo.InvariantCulture);
+
+    public static string SqlNull(this double? value) =>
+        value is null ? Null : value.Value.ToString(CultureInfo.InvariantCulture);
+
+    public static string SqlNull(this int? value) => value is null ? Null : $"{value}";
+    public static string SqlNull(this long? value) => value is null ? Null : $"{value}";
+
+    public static string SqlNull(this bool? value)
+    {
+        return value switch
+        {
+            null => Null,
+            false => "0",
+            true => "1"
+        };
+    }
+}
