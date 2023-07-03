4138eebe9e335704706f4a5e0dd8d8982b9b5cac
TheR7angelo <raphael.antunes@hotmail.fr>
2023-07-03 12:25:45 +0200

    start new control test

---
+++ MyCollectionShelf.Wpf/Shelf/Common/Ui/Controls/LanguageControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using MyCollectionShelf.Wpf.Shelf.Common.Class;
+
+namespace MyCollectionShelf.Wpf.Shelf.Common.Ui.Controls;
+
+public partial class LanguageControl
+{
+    public List<Language> Languages { get; }
+
+    public LanguageControl()
+    {
+        InitializeComponent();
+
+        var availableCultures = GetAvailableCultures();
+        Languages = availableCultures.Select(cultureInfo => new Language
+        {
+            CultureInfo = cultureInfo,
+            DisplayName = LanguageControlResources.ResourceManager.GetString(cultureInfo.EnglishName)!,
+            Name = cultureInfo.Name
+        }).ToList();
+    }
+
+    private static IEnumerable<CultureInfo> GetAvailableCultures()
+    {
+        // Création du gestionnaire de ressources
+        var resourceManager = new ResourceManager(typeof(LanguageControlResources));
+
+        // Obtention des informations relatives aux cultures prises en charge
+        var supportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            .Where(s => !s.Equals(CultureInfo.InvariantCulture));
+        var availableCultures = new List<CultureInfo>();
+        availableCultures.AddRange(supportedCultures.Where(culture =>
+            resourceManager.GetResourceSet(culture, true, false) != null));
+
+        return availableCultures;
+    }
+}
